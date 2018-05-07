8a3beda9c87495d2b394fe4554e90c5ac3dbc94a
TWPLrh <xiangteam013@outlook.com>
2018-05-07 22:48:38 +0800

    Adjust Bones

---
+++ BodyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyController : MonoBehaviour {
+
+    public GameObject[] inGameBones;
+    public GameObject[] KinectBones;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < inGameBones.Length; i++)
+        {
+            var X = KinectBones[i].transform.rotation.eulerAngles.x;
+            var Y = KinectBones[i].transform.rotation.eulerAngles.y;
+            var Z = KinectBones[i].transform.rotation.eulerAngles.z;
+
+            Vector3 eulerangle = new Vector3(0, 0, Z);
+
+            inGameBones[i].transform.eulerAngles = eulerangle;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Image Processing Project
+
+- Usage in Unity
+- OpenCvSharp
+- Anima2D
+- Kinect with MS-SDK
